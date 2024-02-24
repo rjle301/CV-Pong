ff49984c4546714b96604beb36f65fa91158da67
RJLE <rlemboehms@gmail.com>
2024-02-24 18:50:00 -0500

    Implemented ball collisions
    Co-authored-by: Mark Luskiewicz <mlusk02@users.noreply.github.com>
    Co-authored-by: Ethan Battaglia <greagob@users.noreply.github.com>

--- pygame_modules/cursor_drawing.py
+++ pygame_modules/cursor_drawing.py
@@ -1,13 +1,20 @@
 import cv2
 import mediapipe
 import pygame 
-from paddle import *
+from pong_shapes import *
  
+# Window constants
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600 
 
+# Paddle constants
 PADDLE_WIDTH = WINDOW_WIDTH * .02
 PADDLE_HEIGHT = WINDOW_HEIGHT * .15
+
+# Ball constants
+START_X = WINDOW_WIDTH / 2
+START_Y = WINDOW_HEIGHT / 2
+RADIUS = 7
 
 pygame.init()
 
@@ -27,39 +34,48 @@ pygame.display.flip()
 #Initialize two paddles - left and right
 left_paddle = Paddle(30, 30, PADDLE_WIDTH, WINDOW_HEIGHT * .15)
 right_paddle = Paddle(770 - WINDOW_WIDTH * .02, 30, PADDLE_WIDTH, PADDLE_HEIGHT)
+paddle_list = [left_paddle, right_paddle]
+ball = Ball(START_X, START_Y, RADIUS)
   
 # Variable to keep our game loop running 
 running = True
   
 # game loop 
 while running: 
-    #success, img = capture.read()
-    #hand_location = get_hand_location(img)
     
-    
-# for loop through the event queue   
+    # for loop through the event queue   
     for event in pygame.event.get(): 
         # Check for QUIT event       
         if event.type == pygame.QUIT: 
             running = False
 
-
-    screen.fill("black")
-
     cursor_x, cursor_y = pygame.mouse.get_pos()
-    # print([cursor_x, cursor_y])
-    print(pygame.mouse.get_focused())
-    if not pygame.mouse.get_focused():
+    
+    # If the cursor goes out of the game window, or the cursor gets too close to
+    # the bottom of the game window, the paddle is snapped to the bottom of the game window
+    if (not pygame.mouse.get_focused()) or (cursor_y > WINDOW_HEIGHT - PADDLE_HEIGHT):
         if cursor_y > WINDOW_HEIGHT / 2:
-            left_paddle.setY(WINDOW_HEIGHT - PADDLE_HEIGHT)
-            right_paddle.setY(WINDOW_HEIGHT - PADDLE_HEIGHT)
+            left_paddle.set_y(WINDOW_HEIGHT - PADDLE_HEIGHT)
+            right_paddle.set_y(WINDOW_HEIGHT - PADDLE_HEIGHT)
         else:
-            left_paddle.setY(0)
-            right_paddle.setY(0)
+            left_paddle.set_y(0)
+            right_paddle.set_y(0)
     else:
-        left_paddle.setY(cursor_y)
-        right_paddle.setY(cursor_y)
+        left_paddle.set_y(cursor_y)
+        right_paddle.set_y(cursor_y)
         
+    ball.move()
+    
+    for paddle in paddle_list:
+        if (paddle.get_rectangle().colliderect(pygame.Rect(ball.get_x() - ball.get_radius(), ball.get_y() - ball.get_radius(), ball.get_diameter(), ball.get_diameter()))):
+            ball.flip_x_velocity()
+        
+    if (ball.get_y() - ball.get_radius()) <= 0 or (ball.get_y() + ball.get_radius() >= WINDOW_HEIGHT):
+        ball.flip_y_velocity()
+        
+    screen.fill("black")
     left_paddle.draw(screen, "white")
     right_paddle.draw(screen, "white")
+    ball.draw(screen, "white")
     pygame.display.flip()
+
---
+++ pygame_modules/pong_shapes.py
@@ -0,0 +1,54 @@
+import pygame
+
+class Ball:
+    BALL_SPEED = 
+    def __init__(self, x_pos, y_pos, radius):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.radius = radius
+        self.diameter = radius * 2
+        
+        self.velocity_y = .2
+        self.velocity_x = .2
+
+    def get_x(self):
+        return self.x_pos
+    
+    def get_y(self):
+        return self.y_pos
+    
+    def get_radius(self):
+        return self.radius
+    
+    def get_diameter(self):
+        return self.diameter
+    
+    def draw(self, screen, color):
+        pygame.draw.circle(screen, color, (self.x_pos, self.y_pos), self.radius, 0)
+    
+    def move(self):
+        self.x_pos += self.velocity_x
+        self.y_pos += self.velocity_y
+
+    def flip_x_velocity(self):
+        self.velocity_x *= -1
+    
+    def flip_y_velocity(self):
+        self.velocity_y *= -1
+
+
+class Paddle:
+    def __init__(self, x_pos, y_pos, width, height):
+        self.rect = pygame.Rect(x_pos, y_pos, width, height)
+        self.xPos = x_pos
+        self.width = width
+        self.height = height
+
+    def draw(self, screen, color):
+        pygame.draw.rect(screen, color, self.rect)
+
+    def set_y(self, y_pos):
+        self.rect.update(self.xPos, y_pos, self.width, self.height)
+
+    def get_rectangle(self):
+        return self.rect
